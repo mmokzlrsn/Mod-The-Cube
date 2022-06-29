84241ee1dc706ef650ab306b56671e6c93dec45b
Mehmet Ali KIZILARSLAN <76072213+mmokzlrsn@users.noreply.github.com>
2022-06-29 17:57:37 +0300

    WEB GL is uploaded to unity play

---
+++ Mod The Cube/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+   
+
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+        transform.localScale = Vector3.one * Random.Range(1f, 3f);
+        Material material = Renderer.material;
+
+
+        material.color = new Color(colorRand(), colorRand(), colorRand(), colorRand());
+        
+    }
+    
+    void Update()
+    {
+        
+        transform.Rotate(speedRand() * Time.deltaTime, speedRand() * Time.deltaTime, speedRand() * Time.deltaTime);
+        
+    }
+
+    float colorRand()
+    {
+        return Random.Range(0, 1f);
+    }
+    float speedRand()
+    {
+        return Random.Range(0, 300f);
+    }
+}
